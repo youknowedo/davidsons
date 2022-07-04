e176a47abeaa4d4a5025c8ead8d7fcf7eacad5e8
Filip Mårtensson <fenwikk@protonmail.com>
2022-07-04 09:36:01 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Section from "$lib/components/Section.svelte"
+	import working from "$lib/assets/working.gif"
+	import logo from "$lib/assets/DavidsonsB.png"
+</script>
+
+<div class="absolute top-0 w-full">
+	<div class="container flex items-center py-8">
+		<img src={logo} class="h-14" alt="">
+		<h1 class="text-xl font-bold ml-2">Davidsons</h1>
+	</div>
+</div>
+
+<Section class="flex flex-col items-center justify-center h-screen">
+    <img src={working} alt="">
+
+    <h2 class="text-2xl font-bold">Vår sida är under konstruktion</h2>
+    <p class="text-sb-black/50">Vi jobbar med det för att ge dig den bästa upplevelsen!</p>
+</Section> 
